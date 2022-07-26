f4eef806d2323a3c9309ce712eede94b4ff66321
Danil <danil-danil-korolev@bk.ru>
2022-07-26 17:20:34 +0700

    creating sidebar 7

--- components/Sidebar/SidebarContentProfile.tsx
+++ components/Sidebar/SidebarContentProfile.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
-import Link from 'next/link'
+
+import { useState } from 'react'
+import { setDynamicClasses, setStaticClasses } from '../../lib/classes.lib'
 
 import AvatarPicture from '../../public/images/avatar.png'
 
@@ -7,24 +9,41 @@ import styles from '../../styles/modules/Sidebar/SidebarContentProfile.module.sc
 
 const {
   sidebarContentProfile,
-  sidebarContentPorfile__avatar,
-  sidebarContentProfileDetails,
-  sidebarContentProfileDetails__initials,
-  sidebarContentProfileDetailsSpecials,
-  sidebarContentProfileDetailsSpecialsOff,
-  sidebarContentProfileDetailsSpecialsOff__title,
-  sidebarContentProfileDetailsSpecialsOff__value,
-  sidebarContentProfileDetailsSpecialsBonuses,
-  sidebarContentProfileDetailsSpecialsBonuses__title,
-  sidebarContentProfileDetailsSpecialsBonuses__value,
+  sidebarContentProfile__inner,
+  sidebarContentProfileUpper,
+  sidebarContentPorfileUpper__avatar,
+  sidebarContentProfileUpper__name,
+  sidebarContentProfileDowned,
+  sidebarContentProfileDownedSpecials,
+  sidebarContentProfileDownedSpecials__list,
+  sidebarContentProfileDownedSpecialsItem,
+  sidebarContentProfileDownedSpecialsItem__title,
+  sidebarContentProfileDownedSpecialsItem__value,
+  sidebarContentProfileUpper__btn,
+  _isSubmenuOpen,
 } = styles
 
 export const SidebarContentProfile = () => {
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false)
+
+  const toggleSubmenuHandler = () => {
+    setIsSubmenuOpen((prev) => !prev)
+  }
+
   return (
-    <div className={sidebarContentProfile}>
-      <Link href='/'>
-        <a>
-          <div className={sidebarContentPorfile__avatar}>
+    <div
+      className={setDynamicClasses({
+        staticClasses: [sidebarContentProfile],
+        dynamicClasses: [[_isSubmenuOpen]],
+        conditions: [isSubmenuOpen],
+      })}
+    >
+      <div className={sidebarContentProfile__inner}>
+        <div
+          className={sidebarContentProfileUpper}
+          onClick={toggleSubmenuHandler}
+        >
+          <div className={sidebarContentPorfileUpper__avatar}>
             <Image
               src={AvatarPicture.src}
               alt='avatar picture profile'
@@ -33,35 +52,50 @@ export const SidebarContentProfile = () => {
               layout='fixed'
             />
           </div>
-          <div className={sidebarContentProfileDetails}>
-            <div className={sidebarContentProfileDetails__initials}>
-              Ada Wong
-            </div>
-            {/* <div className={sidebarContentProfileDetailsSpecials}>
-              <div className={sidebarContentProfileDetailsSpecialsOff}>
-                <div className={sidebarContentProfileDetailsSpecialsOff__title}>
-                  Your personal customer discount:
+          <div className={sidebarContentProfileUpper__name}>Ada Wong</div>
+          <div className={sidebarContentProfileUpper__btn}>
+            <button type='button'>
+              <svg
+                width='20'
+                height='20'
+                viewBox='0 0 20 20'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='M15.8334 7.50006L10 13.3334L4.16671 7.50006'
+                  stroke='#3A3A3A'
+                  strokeWidth='1.8'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className={sidebarContentProfileDowned}>
+          <div className={sidebarContentProfileDownedSpecials}>
+            <ul className={sidebarContentProfileDownedSpecials__list}>
+              <li className={sidebarContentProfileDownedSpecialsItem}>
+                <div className={sidebarContentProfileDownedSpecialsItem__title}>
+                  Personal customer off:
                 </div>
-                <div className={sidebarContentProfileDetailsSpecialsOff__value}>
+                <div className={sidebarContentProfileDownedSpecialsItem__value}>
                   19%
                 </div>
-              </div>
-              <div className={sidebarContentProfileDetailsSpecialsBonuses}>
-                <div
-                  className={sidebarContentProfileDetailsSpecialsBonuses__title}
-                >
-                  Your bonus points:
+              </li>
+              <li className={sidebarContentProfileDownedSpecialsItem}>
+                <div className={sidebarContentProfileDownedSpecialsItem__title}>
+                  Bonus points:
                 </div>
-                <div
-                  className={sidebarContentProfileDetailsSpecialsBonuses__value}
-                >
+                <div className={sidebarContentProfileDownedSpecialsItem__value}>
                   155
                 </div>
-              </div>
-            </div> */}
+              </li>
+            </ul>
           </div>
-        </a>
-      </Link>
+        </div>
+      </div>
     </div>
   )
 }
