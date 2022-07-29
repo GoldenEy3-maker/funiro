30043c65bcca9a673275f47fd910dc05b2c554ea
Danil <danil-danil-korolev@bk.ru>
2022-07-29 22:26:35 +0700

    craeting intro 1

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -11,6 +11,7 @@ import { setDynamicClasses, setStaticClasses } from '../../lib/classes.lib'
 import styles from '../../styles/modules/Intro/Intro.module.scss'
 
 import Slide1 from '../../public/images/slide1.jpg'
+import { TIntervalTimer } from '../../typescript/type'
 
 interface IIntroSliderData {
   id: string
@@ -38,25 +39,17 @@ const {
   introSlider__list,
   introSliderItem,
   introSlideInfo,
+  introSlideInfo__inner,
   introSlideInfoHead,
   introSlideInfoBody,
   introSlideInfoFooter,
   introSlideInfoFooter__price,
   introSlideInfoFooter__link,
   _activeDot,
+  _isAnimate,
 } = styles
 
 const Intro = () => {
-  const slideWidthRef = useRef(934)
-  const gapSlidesRef = useRef(32)
-  const startPositionRef = useRef(320)
-  const startPointSlideRef = useRef(
-    startPositionRef.current + slideWidthRef.current + gapSlidesRef.current
-  )
-
-  const [activeSlideIndex, setActiveSlideIndex] = useState<number>(0)
-  const [transformX, setTransformX] = useState<number | null>(null)
-
   const sliderDataRef = useRef<IIntroSliderData[]>([
     {
       id: uuidv4(),
@@ -99,9 +92,34 @@ const Intro = () => {
       href: '/',
     },
   ])
+  const slideWidthRef = useRef(934)
+  const gapSlidesRef = useRef(32)
+  const startPositionRef = useRef(320)
+  const startPointSlideRef = useRef(
+    startPositionRef.current + slideWidthRef.current + gapSlidesRef.current
+  )
+  const animTimeoutIDRef = useRef<TIntervalTimer>()
+
+  const [activeSlideIndex, setActiveSlideIndex] = useState<number>(0)
+  const [transformX, setTransformX] = useState<number | null>(null)
+  const [isAnimStart, setIsAnimStart] = useState(false)
+
+  const animationSlideInfoHandler = () => {
+    setIsAnimStart(true)
+
+    if (animTimeoutIDRef.current) {
+      clearTimeout(animTimeoutIDRef.current)
+      animTimeoutIDRef.current = undefined
+    }
+
+    animTimeoutIDRef.current = setTimeout(() => {
+      setIsAnimStart(false)
+    }, 500)
+  }
 
   const nextSlideHandler = () => {
     setActiveSlideIndex((prev) => prev + 1)
+    animationSlideInfoHandler()
 
     if (activeSlideIndex === sliderDataRef.current.length - 1) {
       setTransformX(startPositionRef.current)
@@ -116,6 +134,7 @@ const Intro = () => {
 
   const prevSlideHandler = () => {
     setActiveSlideIndex((prev) => prev - 1)
+    animationSlideInfoHandler()
 
     if (activeSlideIndex === 0) {
       setTransformX(
@@ -136,6 +155,7 @@ const Intro = () => {
     setActiveSlideIndex(
       sliderDataRef.current.findIndex((slide) => slide.id === id)
     )
+    animationSlideInfoHandler()
   }
 
   return (
@@ -223,34 +243,42 @@ const Intro = () => {
           </div>
         </div>
       </div>
-      <div className={introSlideInfo}>
-        <div className={introSlideInfoHead}>
-          {sliderDataRef.current[activeSlideIndex].title}
-        </div>
-        <div className={introSlideInfoBody}>
-          {sliderDataRef.current[activeSlideIndex].description}
-        </div>
-        <div className={introSlideInfoFooter}>
-          <div className={introSlideInfoFooter__price}>
-            {sliderDataRef.current[activeSlideIndex].price}
+      <div
+        className={setDynamicClasses({
+          staticClasses: [introSlideInfo],
+          dynamicClasses: [[_isAnimate]],
+          conditions: [isAnimStart],
+        })}
+      >
+        <div className={introSlideInfo__inner}>
+          <div className={introSlideInfoHead}>
+            {sliderDataRef.current[activeSlideIndex].title}
           </div>
-          <div className={introSlideInfoFooter__link}>
-            <Link href={sliderDataRef.current[activeSlideIndex].href}>
-              <a>
-                <svg
-                  width='16'
-                  height='12'
-                  viewBox='0 0 16 12'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path
-                    d='M15.3652 6.56003L10.3852 11.56C10.2252 11.72 10.0251 11.8 9.82515 11.8C9.62515 11.8 9.42515 11.72 9.26515 11.56C8.94515 11.24 8.94515 10.74 9.26515 10.42L12.8852 6.78003H1.20515C0.765151 6.78003 0.405151 6.42003 0.405151 5.98003C0.405151 5.54003 0.765151 5.18003 1.20515 5.18003H12.8852L9.26515 1.54004C8.94515 1.22004 8.94515 0.720034 9.26515 0.400034C9.58515 0.0800342 10.0852 0.0800342 10.4052 0.400034L15.3852 5.40003C15.6852 5.74003 15.6852 6.26003 15.3652 6.56003Z'
-                    fill='black'
-                  />
-                </svg>
-              </a>
-            </Link>
+          <div className={introSlideInfoBody}>
+            {sliderDataRef.current[activeSlideIndex].description}
+          </div>
+          <div className={introSlideInfoFooter}>
+            <div className={introSlideInfoFooter__price}>
+              {sliderDataRef.current[activeSlideIndex].price}
+            </div>
+            <div className={introSlideInfoFooter__link}>
+              <Link href={sliderDataRef.current[activeSlideIndex].href}>
+                <a>
+                  <svg
+                    width='16'
+                    height='12'
+                    viewBox='0 0 16 12'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M15.3652 6.56003L10.3852 11.56C10.2252 11.72 10.0251 11.8 9.82515 11.8C9.62515 11.8 9.42515 11.72 9.26515 11.56C8.94515 11.24 8.94515 10.74 9.26515 10.42L12.8852 6.78003H1.20515C0.765151 6.78003 0.405151 6.42003 0.405151 5.98003C0.405151 5.54003 0.765151 5.18003 1.20515 5.18003H12.8852L9.26515 1.54004C8.94515 1.22004 8.94515 0.720034 9.26515 0.400034C9.58515 0.0800342 10.0852 0.0800342 10.4052 0.400034L15.3852 5.40003C15.6852 5.74003 15.6852 6.26003 15.3652 6.56003Z'
+                      fill='black'
+                    />
+                  </svg>
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
